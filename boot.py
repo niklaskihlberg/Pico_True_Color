4122958ce642a16f13d62cb0d4eb6930b6094d88
nklz <niklaskihlberg@me.com>
2023-05-13 00:20:17 +0200

    Update

--- boot.py
+++ boot.py
@@ -1,5 +1,17 @@
+import board
 import usb_midi  # usb_hid
+
+from digitalio import DigitalInOut, Direction, Pull
 
 # On some boards, we need to give up HID to accomodate MIDI.
 # usb_hid.enable()
 usb_midi.enable()
+
+# DigitalInOut(board.GP2).direction = Direction.INPUT
+# DigitalInOut(board.GP2).pull = Pull.UP
+#
+# if not DigitalInOut(board.GP2).value:
+#     MOUNT THE STORAGE
+#
+# else:
+#     HIDE IT...
--- code.py
+++ code.py
@@ -1,10 +1,10 @@
 import board
 import usb_midi
 import adafruit_midi
-
-from digitalio import DigitalInOut, Direction, Pull
+import time
 import rotaryio
 
+from digitalio import DigitalInOut, Direction, Pull
 from adafruit_midi.pitch_bend import PitchBend
 
 midi = adafruit_midi.MIDI(
@@ -15,43 +15,34 @@ midi = adafruit_midi.MIDI(
 e1b = DigitalInOut(board.GP2)
 e1b.direction = Direction.INPUT
 e1b.pull = Pull.UP
-
-e1b_last = e1b.value
-
+last_e1b = True
 
 # ENCODER 1 ### DEF ###
 e1r = rotaryio.IncrementalEncoder(board.GP0, board.GP1)
-e1r_last = 0
-
+last_e1r = 0
 
 while True:
 
-    # ENCODER 1 BTN ###
-    if e1b.value != e1b_last:
-        if not e1b.value:
-            print("E1 Button - PRESSED - ", e1b.value)
-        else:
-            print("E1 Button - RELEASED - ", e1b.value)
-
-    e1b_last = e1b.value
-
     # ENCODER 1 ### HUE #### 0 # 8192 # 16383 #####
-    if e1r_last is None or e1r.position != e1r_last:
+    if e1r.position != last_e1r:
 
-        if e1r.position >= 16384:
-            e1r.position = 0
+        if e1b.value: # "NORMAL MODE"
+            if e1r.position > last_e1r: e1r.position = e1r.position + 9
+            else: e1r.position = e1r.position - 9
 
-        if e1r.position <= -1:
-            e1r.position = 16383
-        
-        midi.send(PitchBend(e1r.position))
-        print("Enc 1,    position: ", e1r.position)
-        
-        # if not e1b.value:
-            # midi.send(PitchBend(int(e1r.position * 100)))
-            # print("Enc 1,    position: ", int(e1r.position * 100))
-        # else:
-            # midi.send(PitchBend(int(e1r.position * 10)))
-            # print("Enc 1,    position: ", int(e1r.position * 10))
-            
-        e1r_last = e1r.position
+        else:  # ENCODER PRESSED
+            if e1r.position > last_e1r: e1r.position = e1r.position + 249
+            else: e1r.position = e1r.position - 249
+
+        e1r.position = (e1r.position + 16384) % 16384
+        midi.send(PitchBend(e1r.position), channel=0)
+        e1b_last = e1b.value
+        last_e1r = e1r.position
+
+        print("Color: ", e1r.position)
+        print("Button: ", e1b.value)
+        print(" ")
+
+    time.sleep(0.001)
+    # time.sleep(0.025)
+    # time.sleep(0.5)
